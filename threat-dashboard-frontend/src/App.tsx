79603c23ee8ae278ad409ea884bf5d35ec1f860a
unknown <ensar.hujdur196@gmail.com>
2026-03-03 02:34:39 +0100

    02:34

--- threat-dashboard-frontend/src/App.tsx
+++ threat-dashboard-frontend/src/App.tsx
@@ -2,10 +2,8 @@ import './App.css'
 
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Tailwind v4 is working 🚀
-      </h1>
-    </div>
-  )
+        <h1 className="text-4xl font-bold text-blue-500 text-right">
+          Tailwind v4 <br />is working 🚀
+        </h1>
+  );
 }
